8b25c8b8528303a9a91b7f1016dbb6a32cb1c187
matei <mateiprodan1@gmail.com>
2024-11-09 09:56:34 +0200

    09.11.2024 - 09:55 Changed some variable names

--- rescue_now_app/test/widget_test.dart
+++ rescue_now_app/test/widget_test.dart
@@ -6,7 +6,7 @@ import 'package:rescue_now_app/crash_detection.dart'; // Update this to the corr
 void main() {
   testWidgets('Crash Detection App Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(CrashDetectionApp());
+    await tester.pumpWidget(CrashDetectionScreen());
 
     // Verify that the app shows the initial UI elements.
     expect(find.text('Current Acceleration:'), findsOneWidget);
